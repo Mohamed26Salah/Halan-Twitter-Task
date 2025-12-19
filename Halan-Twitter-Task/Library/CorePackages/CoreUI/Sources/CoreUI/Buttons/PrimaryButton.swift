276e8280b45e26ceecd6090d924438d7ca9e34d0
Mohamed26Salah <76624455+Mohamed26Salah@users.noreply.github.com>
2025-12-19 22:28:29 +0200

    add coreUI package + swiftgen

---
+++ Halan-Twitter-Task/Library/CorePackages/CoreUI/Sources/CoreUI/Buttons/PrimaryButton.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  CoreUI
+//
+//  Created by Mohamed Salah on 19/12/2025.
+//
+
+import Foundation
+import SwiftUI
+
+struct PrimaryButton: View {
+    var body: some View {
+        Text("HelloWorld")
+    }
+}
+
+#Preview {
+    PrimaryButton()
+}
